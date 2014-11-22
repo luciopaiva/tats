cebf50079d46deeddedf2dbb35df845add8bb041
Lucio Paiva <luciopaiva@gmail.com>
2014-11-22 21:04:45 -0200

    Initial commit

---
+++ tats.js
@@ -0,0 +1,110 @@
+#! /usr/bin/env node
+
+var
+    spawn = require('child_process').spawn,
+    minimist = require('minimist'),
+    chalk = require('chalk'),
+    async = require('async'),
+    traverse = require('./lib/traverse');
+
+
+function usage() {
+
+    console.info('Usage: node tats <folder>');
+}
+
+function stat(filename, callback) {
+    var
+        result = '',
+        command, params, options,
+        child;
+
+    command = 'stat';
+    params = ['-f', '%a %m %c %B %z %N', filename];
+    options = {
+        stdio: ['ignore', 'pipe', 'ignore']
+    };
+
+    child = spawn(command, params, options);
+
+    child.stdout.setEncoding('utf8');
+
+    child.stdout.on('data', function (data) {
+        result += data;
+    });
+
+    child.on('close', function (code) {
+
+        if (code !== 0) {
+
+            callback('Spawn exited with code "' + code + '" for "' + filename + '".');
+
+        } else {
+
+            callback(null, result.trim());
+        }
+    });
+}
+
+function main(args) {
+    var
+        files,
+        stats = {};
+
+    if (args._.length < 1) {
+
+        usage();
+
+    } else {
+
+        files = traverse(args._[0]);
+
+        // keep the number of max simultaneous open files equal to 10, otherwise EMFILE error will happen
+        async.eachLimit(files, 10, iterateFile, complete);
+    }
+
+    function complete(err) {
+        var
+            names;
+
+        if (err) {
+
+            throw new Error(err);
+
+        } else {
+
+            names = Object.keys(stats).sort();
+
+            console.info('# <access in unix time> <modified in unix time> <change in unix time> <birth in unix time> <size in bytes> <filename>');
+
+            names.forEach(function (filename) {
+
+                console.info(stats[filename]);
+            });
+        }
+    }
+
+    function iterateFile(filename, callback) {
+
+        stat(filename, function registarStat(err, stat) {
+
+            if (err) {
+
+                callback(err);
+
+            } else {
+
+                stats[filename] = stat;
+                callback();
+            }
+        });
+    }
+
+}
+
+function processArgs() {
+
+    return minimist(process.argv.slice(2));
+}
+
+main(processArgs());
